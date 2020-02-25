ef8d30d6742acefa3aa35e502cc695be42d197af
richardswei <wei.richard.s@gmail.com>
2020-02-25 01:58:22 -0800

    wow that's moist.

--- app/javascript/packs/components/Main.js
+++ app/javascript/packs/components/Main.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import NoteList from "./NoteList";
 
-class Main extends React.Component {
+class Dashboard extends React.Component {
+  
   render() {
-    return <div className="main-panel-dash">Main</div>
+    return (
+      <div className="main-panel-dash">
+        <h2>Notes Dashboard</h2>
+        <NoteList/>
+      </div>
+    )
   }
 }
 
 document.addEventListener('turbolinks:load', () => {
   const app = document.getElementById('main')
-  app && ReactDOM.render(<Main />, app)
+  app && ReactDOM.render(<Dashboard />, app)
 })
